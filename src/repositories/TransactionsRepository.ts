4c81d2f59b5efb67fdc4c40376de38518849ced3
danielmorita <danielkeyti@gmail.com>
2020-04-17 13:29:53 -0300

    outro jeito de fazer o reduce

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -18,23 +18,45 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    const list = [...this.transactions];
-    const reducerIncome = (prev: number, cur: Transaction): number => {
-      return prev + (cur.type === 'income' ? cur.value : 0);
-    };
-    const income = list.reduce(reducerIncome, 0);
-    const reducerOutcome = (prev: number, cur: Transaction): number => {
-      return prev + (cur.type === 'outcome' ? cur.value : 0);
-    };
-    const outcome = list.reduce(reducerOutcome, 0);
+    // const list = [...this.transactions];
+    // const reducerIncome = (prev: number, cur: Transaction): number => {
+    //   return prev + (cur.type === 'income' ? cur.value : 0);
+    // };
+    // const income = list.reduce(reducerIncome, 0);
+    // const reducerOutcome = (prev: number, cur: Transaction): number => {
+    //   return prev + (cur.type === 'outcome' ? cur.value : 0);
+    // };
+    // const outcome = list.reduce(reducerOutcome, 0);
 
-    const balance = {
-      income,
-      outcome,
-      total: income - outcome,
-    };
+    // const balance = {
+    //   income,
+    //   outcome,
+    //   total: income - outcome,
+    // };
 
-    return balance;
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
   public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
